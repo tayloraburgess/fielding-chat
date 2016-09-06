86ae07432ebd790b301afa0f6e50b58eec11ea3d
Taylor Burgess <tayloraburgess@gmail.com>
2016-09-06 16:53:11 -0400

    Add error handling and tests for bad HTTP methods
    Add an all route to server that checks for allowed HTTP methods on
    a resource (currently hardcoded to just GET on the API root). If the
    client uses a different verb, route to the error-handling middleware
    at the end of the server code (which will return 405 and customized
    error in response to the client).
    
    Add endpointMethods() function, which takes an endpoint and a list
    of methods and asserts that all other common HTTP methods not in that
    list should get a 405 response.

--- test/test.js
+++ test/test.js
@@ -9,7 +9,7 @@ const chaiHTTP = require('chai-http');
 chai.use(chaiHTTP);
 const should = chai.should();
 
-function endpointBehavior(endpoint) {
+function endpointMedia(endpoint) {
   it('should be able to respond with a body of type application/hal+json', (done) => {
     chai.request(app)
     .get(endpoint)
@@ -24,15 +24,15 @@ function endpointBehavior(endpoint) {
       links.self.should.have.property('href');
       links.self.href.should.be.a('string');
       links.self.href.should.equal(endpoint);
-      Object.values(links).forEach((prop) => {
-        if (Array.isArray(prop)) {
-          prop.forEach((link) => {
+      Object.values(links).forEach((rel) => {
+        if (Array.isArray(rel)) {
+          rel.forEach((link) => {
             link.should.have.property('href');
             link.href.should.be.a('string');
           });
         } else {
-          prop.should.have.property('href');
-          prop.href.should.be.a('string');
+          rel.should.have.property('href');
+          rel.href.should.be.a('string');
         }
       });
       done();
@@ -50,7 +50,7 @@ function endpointBehavior(endpoint) {
   });
 }
 
-function idempotent(endpoint) {
+function endpointIdempotent(endpoint) {
   it('should respond with the same representation if requested multiple times', (done) => {
     let firstReqBody;
     let firstReqStatus;
@@ -72,9 +72,32 @@ function idempotent(endpoint) {
   });
 }
 
+function endpointMethods(endpoint, methods) {
+  const allMethods = {
+    OPTIONS: chai.request(app).options(endpoint),
+    GET: chai.request(app).get(endpoint),
+    POST: chai.request(app).post(endpoint),
+    PUT: chai.request(app).put(endpoint),
+    DELETE: chai.request(app).delete(endpoint),
+    TRACE: chai.request(app).trace(endpoint),
+  };
+  Object.keys(allMethods)
+  .filter((method) => { return methods.indexOf(method) === -1 ? method : false; })
+  .forEach((method) => {
+    it('should respond with status code 405 to ${method}', (done) => {
+      allMethods[method]
+      .end((err) => {
+        err.should.have.status(405);
+        done();
+      });
+    });
+  });
+}
+
 describe('/api/v1', () => {
   describe('GET', () => {
-    endpointBehavior('/api/v1');
-    idempotent('/api/v1');
+    endpointMedia('/api/v1');
+    endpointIdempotent('/api/v1');
   });
+  endpointMethods('/api/v1/', ['GET']);
 });
