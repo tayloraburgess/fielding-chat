d3402acef915b149cd57acb45a09220b7e582a99
Taylor Burgess <tayloraburgess@gmail.com>
2016-09-08 13:57:32 -0400

    Add error handling to all db queries within the API

--- src/app.js
+++ src/app.js
@@ -8,6 +8,8 @@ function customError(status, methods, next, body) {
   let throwErr;
   if (status === 406) {
     throwErr = new Error('Invalid hypermedia type. Try Accept: "application/hal+json" instead.');
+  } else if (status === 500) {
+    throwErr = new Error('The server failed to process your request--likely a database error. Our bad.');
   } else {
     throwErr = new Error(body);
   }
@@ -59,20 +61,24 @@ app.all('/api/v1/users', (req, res, next) => {
 app.get('/api/v1/users', (req, res, next) => {
   if (req.accepts(['application/hal+json', 'application/json', 'json'])) {
     db.getUsers((err, users) => {
-      const items = users.map((user) => {
-        return { href: `/api/v1/users/${user.name}` };
-      });
-      res.status(200)
-      .set({
-        'Content-Type': 'application/hal+json',
-        Allow: 'GET, POST',
-      })
-      .json({
-        _links: {
-          self: { href: '/api/v1/users' },
-          item: items,
-        },
-      });
+      if (err) {
+        customError(500, 'GET, POST', next);
+      } else {
+        const items = users.map((user) => {
+          return { href: `/api/v1/users/${user.name}` };
+        });
+        res.status(200)
+        .set({
+          'Content-Type': 'application/hal+json',
+          Allow: 'GET, POST',
+        })
+        .json({
+          _links: {
+            self: { href: '/api/v1/users' },
+            item: items,
+          },
+        });
+      }
     });
   } else {
     customError(406, 'GET, POST', next);
@@ -90,31 +96,35 @@ app.get('/api/v1/users/:name', (req, res, next) => {
   if (req.accepts(['application/hal+json', 'application/json', 'json'])) {
     db.getUserByName(req.params.name, (err, user) => {
       if (err) {
-        customError(404, 'GET, PUT DELETE', next, `${req.params.name} isn't an existing log.`);
+        customError(404, 'GET, PUT, DELETE', next, `${req.params.name} isn't an existing log.`);
       } else {
         db.getMessages((err2, messages) => {
-          const messageItems = messages.filter((message) => {
-            if (message.user_id.toString() === user._id.toString()) {
-              return message;
-            }
-            return false;
-          }).map((message) => {
-            return { href: `/api/v1/messages/${message.ref_id}` };
-          });
-          res.status(200)
-          .set({
-            'Content-Type': 'application/hal+json',
-            Allow: 'GET, PUT, DELETE',
-          })
-          .json({
-            _links: {
-              self: { href: `/api/v1/users/${req.params.name}` },
-              collection: { href: '/api/v1/users' },
-              related: messageItems,
-            },
-            name: user.name,
-            createdAt: user.created_at,
-          });
+          if (err2) {
+            customError(500, 'GET, PUT, DELETE', next);
+          } else {
+            const messageItems = messages.filter((message) => {
+              if (message.user_id.toString() === user._id.toString()) {
+                return message;
+              }
+              return false;
+            }).map((message) => {
+              return { href: `/api/v1/messages/${message.ref_id}` };
+            });
+            res.status(200)
+            .set({
+              'Content-Type': 'application/hal+json',
+              Allow: 'GET, PUT, DELETE',
+            })
+            .json({
+              _links: {
+                self: { href: `/api/v1/users/${req.params.name}` },
+                collection: { href: '/api/v1/users' },
+                related: messageItems,
+              },
+              name: user.name,
+              createdAt: user.created_at,
+            });
+          }
         });
       }
     });
@@ -133,20 +143,24 @@ app.all('/api/v1/messages', (req, res, next) => {
 app.get('/api/v1/messages', (req, res, next) => {
   if (req.accepts(['application/hal+json', 'application/json', 'json'])) {
     db.getMessages((err, messages) => {
-      const items = messages.map((message) => {
-        return { href: `/api/v1/messages/${message.ref_id}` };
-      });
-      res.status(200)
-      .set({
-        'Content-Type': 'application/hal+json',
-        Allow: 'GET, POST',
-      })
-      .json({
-        _links: {
-          self: { href: '/api/v1/messages' },
-          item: items,
-        },
-      });
+      if (err) {
+        customError(500, 'GET, POST', next);
+      } else {
+        const items = messages.map((message) => {
+          return { href: `/api/v1/messages/${message.ref_id}` };
+        });
+        res.status(200)
+        .set({
+          'Content-Type': 'application/hal+json',
+          Allow: 'GET, POST',
+        })
+        .json({
+          _links: {
+            self: { href: '/api/v1/messages' },
+            item: items,
+          },
+        });
+      }
     });
   } else {
     customError(406, 'GET, POST', next);
@@ -167,35 +181,43 @@ app.get('/api/v1/messages/:ref_id', (req, res, next) => {
         customError(404, 'GET, PUT DELETE', next, `${req.params.ref_id} isn't an existing log.`);
       } else {
         db.getUserById(message.user_id, (err2, userRes) => {
-          db.getLogs((err3, logsRes) => {
-            const logItems = logsRes.filter((log) => {
-              const strMsgIds = log.message_ids.map((id) => {
-                return id.toString();
-              });
-              if (strMsgIds.indexOf(message._id.toString()) > -1) {
-                return log;
+          if (err2) {
+            customError(500, 'GET, PUT, DELETE', next);
+          } else {
+            db.getLogs((err3, logsRes) => {
+              if (err3) {
+                customError(500, 'GET, PUT, DELETE', next);
+              } else {
+                const logItems = logsRes.filter((log) => {
+                  const strMsgIds = log.message_ids.map((id) => {
+                    return id.toString();
+                  });
+                  if (strMsgIds.indexOf(message._id.toString()) > -1) {
+                    return log;
+                  }
+                  return false;
+                }).map((log) => {
+                  return { href: `/api/v1/logs/${log.name}` };
+                });
+                logItems.unshift({ href: 'api/v1/messages' });
+                res.status(200)
+                .set({
+                  'Content-Type': 'application/hal+json',
+                  Allow: 'GET, PUT, DELETE',
+                })
+                .json({
+                  _links: {
+                    self: { href: `/api/v1/messages/${req.params.ref_id}` },
+                    collection: logItems,
+                    related: { href: `/api/v1/users/${userRes.name}` },
+                  },
+                  user: userRes.name,
+                  text: message.text,
+                  createdAt: message.created_at,
+                });
               }
-              return false;
-            }).map((log) => {
-              return { href: `/api/v1/logs/${log.name}` };
             });
-            logItems.unshift({ href: 'api/v1/messages' });
-            res.status(200)
-            .set({
-              'Content-Type': 'application/hal+json',
-              Allow: 'GET, PUT, DELETE',
-            })
-            .json({
-              _links: {
-                self: { href: `/api/v1/messages/${req.params.ref_id}` },
-                collection: logItems,
-                related: { href: `/api/v1/users/${userRes.name}` },
-              },
-              user: userRes.name,
-              text: message.text,
-              createdAt: message.created_at,
-            });
-          });
+          }
         });
       }
     });
@@ -214,20 +236,24 @@ app.all('/api/v1/logs', (req, res, next) => {
 app.get('/api/v1/logs', (req, res, next) => {
   if (req.accepts(['application/hal+json', 'application/json', 'json'])) {
     db.getLogs((err, logs) => {
-      const items = logs.map((log) => {
-        return { href: `/api/v1/logs/${log.name}` };
-      });
-      res.status(200)
-      .set({
-        'Content-Type': 'application/hal+json',
-        Allow: 'GET, POST',
-      })
-      .json({
-        _links: {
-          self: { href: '/api/v1/logs' },
-          item: items,
-        },
-      });
+      if (err) {
+        customError(500, 'GET, POST', next);
+      } else {
+        const items = logs.map((log) => {
+          return { href: `/api/v1/logs/${log.name}` };
+        });
+        res.status(200)
+        .set({
+          'Content-Type': 'application/hal+json',
+          Allow: 'GET, POST',
+        })
+        .json({
+          _links: {
+            self: { href: '/api/v1/logs' },
+            item: items,
+          },
+        });
+      }
     });
   } else {
     customError(406, 'GET, POST', next);
@@ -248,55 +274,63 @@ app.get('/api/v1/logs/:name', (req, res, next) => {
         customError(404, 'GET, PUT DELETE', next, `${req.params.name} isn't an existing log.`);
       } else {
         db.getUsers((err2, usersRes) => {
-          db.getMessages((err3, msgsRes) => {
-            const strMsgIds = log.message_ids.map((id) => {
-              return id.toString();
-            });
-            const strUserIds = log.user_ids.map((id) => {
-              return id.toString();
-            });
-            const msgItems = msgsRes.filter((message) => {
-              if (strMsgIds.indexOf(message._id.toString()) > -1) {
-                return message;
-              }
-              return false;
-            }).map((message) => {
-              return message.ref_id.toString();
-            });
-            const userItems = usersRes.filter((user) => {
-              if (strUserIds.indexOf(user._id.toString()) > -1) {
-                return user;
-              }
-              return false;
-            }).map((user) => {
-              return user.name;
-            });
+          if (err2) {
+            customError(500, 'GET, PUT, DELETE', next);
+          } else {
+            db.getMessages((err3, msgsRes) => {
+              if (err3) {
+                customError(500, 'GET, PUT, DELETE', next);
+              } else {
+                const strMsgIds = log.message_ids.map((id) => {
+                  return id.toString();
+                });
+                const strUserIds = log.user_ids.map((id) => {
+                  return id.toString();
+                });
+                const msgItems = msgsRes.filter((message) => {
+                  if (strMsgIds.indexOf(message._id.toString()) > -1) {
+                    return message;
+                  }
+                  return false;
+                }).map((message) => {
+                  return message.ref_id.toString();
+                });
+                const userItems = usersRes.filter((user) => {
+                  if (strUserIds.indexOf(user._id.toString()) > -1) {
+                    return user;
+                  }
+                  return false;
+                }).map((user) => {
+                  return user.name;
+                });
 
-            const items = userItems.map((user) => {
-              return { href: `/api/v1/users/${user}` };
-            }).concat(
-              msgItems.map((msg) => {
-                return { href: `/api/v1/messages/${msg}` };
-              })
-            );
+                const items = userItems.map((user) => {
+                  return { href: `/api/v1/users/${user}` };
+                }).concat(
+                  msgItems.map((msg) => {
+                    return { href: `/api/v1/messages/${msg}` };
+                  })
+                );
 
-            res.status(200)
-            .set({
-              'Content-Type': 'application/hal+json',
-              Allow: 'GET, POST',
-            })
-            .json({
-              _links: {
-                self: { href: `/api/v1/logs/${req.params.name}` },
-                collection: { href: 'api/v1/logs' },
-                related: items,
-              },
-              name: log.name,
-              users: userItems,
-              messages: msgItems,
-              createdAt: log.created_at,
+                res.status(200)
+                .set({
+                  'Content-Type': 'application/hal+json',
+                  Allow: 'GET, POST',
+                })
+                .json({
+                  _links: {
+                    self: { href: `/api/v1/logs/${req.params.name}` },
+                    collection: { href: 'api/v1/logs' },
+                    related: items,
+                  },
+                  name: log.name,
+                  users: userItems,
+                  messages: msgItems,
+                  createdAt: log.created_at,
+                });
+              }
             });
-          });
+          }
         });
       }
     });
