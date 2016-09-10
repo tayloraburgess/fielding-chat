8c1972fce0aa6ac517a0e206ca3544f249dade93
Taylor Burgess <tayloraburgess@gmail.com>
2016-09-10 16:35:15 -0400

    Modify db delete methods to delete associations
    deleteUser() now deletes any associated messages and removes the
    user from any associated logs.
    
    deleteMessage() now removes the message from any associated logs.
    
    Refactor db methods generally to be more concise. Add several more
    helper methods to facilate thorough deletion, and test them.
    
    Update README.md to reflect how deletion now works.

--- src/db.js
+++ src/db.js
@@ -120,31 +120,105 @@ export function createUser(name, callback = null) {
 }
 
 export function updateUserName(userId, newName, callback = null) {
-  const query = { _id: userId };
-  const newData = { name: newName };
-  User.update(query, newData, {}, (err, res) => {
-    if (callback) {
-      if (err) {
-        callback(err, null);
-      } else {
-        callback(null, res);
-      }
+  getUserById(userId, (err1, userRes) => {
+    if (err1 && callback) {
+      callback(err1);
+    } else {
+      User.update({ _id: userRes._id }, { name: newName }, {}, (err2, res) => {
+        if (callback) {
+          if (err2) {
+            callback(err2, null);
+          } else {
+            callback(null, res);
+          }
+        }
+      });
+    }
+  });
+}
+
+export function removeUserFromLog(logId, userId, callback = null) {
+  getUserById(userId, (err1, userRes) => {
+    if (err1 && callback) {
+      callback(err1);
+    } else {
+      getLogById(logId, (err2, logRes) => {
+        if (err2 && callback) {
+          callback(err2);
+        } else {
+          Log.update({ _id: logRes._id }, { $pull: { user_ids: userRes._id } }, (err3) => {
+            if (callback & err3) {
+              callback(err3);
+            } else {
+              callback(null);
+            }
+          });
+        }
+      });
+    }
+  });
+}
+
+export function removeUserFromAllLogs(userId, callback = null) {
+  getUserById(userId, (err1, userRes) => {
+    if (err1 && callback) {
+      callback(err1);
+    } else {
+      Log.update({ }, { $pull: { user_ids: userRes._id } }, { multi: true }, (err2) => {
+        if (callback & err2) {
+          callback(err2);
+        } else {
+          callback(null);
+        }
+      });
+    }
+  });
+}
+
+export function deleteUserMessages(userId, callback = null) {
+  getUserById(userId, (err1, userRes) => {
+    if (err1 && callback) {
+      callback(err1);
+    } else {
+      Message.remove({ user_id: userRes._id }, (err2) => {
+        if (callback & err2) {
+          callback(err2);
+        } else {
+          callback(null);
+        }
+      });
     }
   });
 }
 
 export function deleteUser(userId, callback = null) {
-  getUserById(userId, (err1) => {
+  getUserById(userId, (err1, userRes) => {
     if (err1 & callback) {
       callback(err1);
     } else {
-      User.remove({ _id: userId }, (err2) => {
-        if (callback) {
-          if (err2) {
-            callback(err2);
-          } else {
-            callback(null);
-          }
+      removeMessageFromAllLogs(userRes.message_ids, (err2) => {
+        if (callback && err2) {
+          callback(err2);
+        } else {
+          deleteUserMessages(userRes._id, (err3) => {
+            if (callback && err3) {
+              callback(err3);
+            } else {
+              removeUserFromAllLogs(userRes._id, (err4) => {
+                if (callback && err4) {
+                  callback(err4);
+                } else {
+                  User.remove({ _id: userRes._id }, (err5) => {
+                    if (err5 && callback) {
+                      callback(err5);
+                    } else {
+                      callback(null);
+                    }
+                  });
+                }
+              });
+            }
+          });
         }
       });
     }
@@ -162,32 +236,6 @@ export function getMessages(callback = null) {
         callback(null, messages);
       }
     }
-  });
-}
-
-export function createMessage(userId, text, callback = null) {
-  getMessages((err1, res) => {
-    let refId;
-    if (res.length === 0) {
-      refId = 1;
-    } else {
-      refId = res[res.length - 1].ref_id + 1;
-    }
-    const newMessage = new Message({
-      user_id: userId,
-      ref_id: refId,
-      text,
-      created_at: new Date(),
-    });
-    newMessage.save((err2, resMessage) => {
-      if (callback) {
-        if (err2) {
-          callback(err2, null);
-        } else {
-          callback(null, resMessage);
-        }
-      }
-    });
   });
 }
 
@@ -225,46 +273,132 @@ export function getMessageByRefId(refId, callback = null) {
   });
 }
 
-export function updateMessageText(messageId, newText, callback = null) {
-  const query = { _id: messageId };
-  const newData = { text: newText };
-  Message.update(query, newData, {}, (err, res) => {
-    if (callback) {
-      if (err) {
-        callback(err, null);
-      } else {
-        callback(null, res);
-      }
+export function createMessage(userId, text, callback = null) {
+  getMessages((err1, res) => {
+    let refId;
+    if (res.length === 0) {
+      refId = 1;
+    } else {
+      refId = res[res.length - 1].ref_id + 1;
     }
+    const newMessage = new Message({
+      user_id: userId,
+      ref_id: refId,
+      text,
+      created_at: new Date(),
+    });
+    newMessage.save((err2, resMessage) => {
+      if (callback) {
+        if (err2) {
+          callback(err2, null);
+        } else {
+          callback(null, resMessage);
+        }
+      }
+    });
   });
 }
 
-export function updateMessageUser(messageId, newUser, callback = null) {
-  const query = { _id: messageId };
-  const newData = { user_id: newUser };
-  Message.update(query, newData, {}, (err, res) => {
-    if (callback) {
-      if (err) {
-        callback(err, null);
-      } else {
-        callback(null, res);
-      }
-    }
-  });
-}
-
-export function deleteMessage(messageId, callback = null) {
-  getMessageById(messageId, (err1) => {
+export function updateMessageText(msgId, newText, callback = null) {
+  getMessageById(msgId, (err1, msgRes) => {
     if (err1 & callback) {
       callback(err1);
     } else {
-      Message.remove({ _id: messageId }, (err2) => {
+      Message.update({ _id: msgRes._id }, { text: newText }, {}, (err2, res) => {
         if (callback) {
           if (err2) {
-            callback(err2);
+            callback(err2, null);
           } else {
-            callback(null);
+            callback(null, res);
           }
+        }
+      });
+    }
+  });
+}
+
+export function updateMessageUser(msgId, userId, callback = null) {
+  getMessageById(msgId, (err1, msgRes) => {
+    if (err1 & callback) {
+      callback(err1);
+    } else {
+      getUserById(userId, (err2, userRes) => {
+        if (err2 & callback) {
+          callback(err2);
+        } else {
+          Message.update({ _id: msgRes._id }, { user_id: userRes._id }, {}, (err3, res) => {
+            if (callback) {
+              if (err3) {
+                callback(err3, null);
+              } else {
+                callback(null, res);
+              }
+            }
+          });
+        }
+      });
+    }
+  });
+}
+
+export function removeMessageFromLog(logId, msgId, callback = null) {
+  getMessageById(msgId, (err1, msgRes) => {
+    if (err1 & callback) {
+      callback(err1);
+    } else {
+      getLogById(logId, (err2, logRes) => {
+        if (err2 & callback) {
+          callback(err2);
+        } else {
+          Log.update({ _id: logRes._id }, { $pull: { message_ids: msgRes._id } }, (err) => {
+            if (callback & err) {
+              callback(err);
+            } else {
+              callback(null);
+            }
+          });
+        }
+      });
+    }
+  });
+}
+
+export function removeMessageFromAllLogs(msgId, callback = null) {
+  if (Array.isArray(msgId)) {
+    Log.update({ }, { $pullAll: { message_ids: msgId } }, { multi: true }, (err) => {
+      if (callback & err) {
+        callback(err);
+      } else {
+        callback(null);
+      }
+    });
+  } else {
+    Log.update({ }, { $pull: { message_ids: msgId } }, { multi: true }, (err) => {
+      if (callback & err) {
+        callback(err);
+      } else {
+        callback(null);
+      }
+    });
+  }
+}
+
+export function deleteMessage(msgId, callback = null) {
+  getMessageById(msgId, (err1, msgRes) => {
+    if (err1 & callback) {
+      callback(err1);
+    } else {
+      removeMessageFromAllLogs(msgRes._id, (err2) => {
+        if (err2 && callback) {
+          callback(err2);
+        } else {
+          Message.remove({ _id: msgRes._id }, (err3) => {
+            if (callback & err3) {
+              callback(err3);
+            } else {
+              callback(null);
+            }
+          });
         }
       });
     }
@@ -338,59 +472,69 @@ export function getLogByName(name, callback = null) {
 }
 
 export function updateLogName(logId, newName, callback = null) {
-  const query = { _id: logId };
-  const newData = { name: newName };
-  Log.update(query, newData, {}, (err, res) => {
-    if (callback) {
-      if (err) {
-        callback(err, null);
-      } else {
-        callback(null, res);
-      }
+  getLogById(logId, (err1, logRes) => {
+    if (err1 & callback) {
+      callback(err1);
+    } else {
+      Log.update({ _id: logRes._id }, { name: newName }, {}, (err2, res) => {
+        if (callback) {
+          if (err2) {
+            callback(err2, null);
+          } else {
+            callback(null, res);
+          }
+        }
+      });
     }
   });
 }
 
 export function updateLogUsers(logId, newUsers, callback = null) {
-  const query = { _id: logId };
-  const newData = { user_ids: newUsers };
-  Log.update(query, newData, {}, (err, res) => {
-    if (callback) {
-      if (err) {
-        callback(err, null);
-      } else {
-        callback(null, res);
-      }
+  getLogById(logId, (err1, logRes) => {
+    if (err1 & callback) {
+      callback(err1);
+    } else {
+      Log.update({ _id: logRes._id }, { user_ids: newUsers }, {}, (err2, res) => {
+        if (callback) {
+          if (err2) {
+            callback(err2, null);
+          } else {
+            callback(null, res);
+          }
+        }
+      });
     }
   });
 }
 
 export function updateLogMessages(logId, newMessages, callback = null) {
-  const query = { _id: logId };
-  const newData = { message_ids: newMessages };
-  Log.update(query, newData, {}, (err, res) => {
-    if (callback) {
-      if (err) {
-        callback(err, null);
-      } else {
-        callback(null, res);
-      }
+  getLogById(logId, (err1, logRes) => {
+    if (err1 & callback) {
+      callback(err1);
+    } else {
+      Log.update({ _id: logRes._id }, { message_ids: newMessages }, {}, (err2, res) => {
+        if (callback) {
+          if (err2) {
+            callback(err2, null);
+          } else {
+            callback(null, res);
+          }
+        }
+      });
     }
   });
 }
 
 export function deleteLog(logId, callback = null) {
-  getLogById(logId, (err1) => {
+  getLogById(logId, (err1, logRes) => {
     if (err1 & callback) {
       callback(err1);
     } else {
-      Log.remove({ _id: logId }, (err2) => {
-        if (callback) {
-          if (err2) {
-            callback(err2);
-          } else {
-            callback(null);
-          }
+      Log.remove({ _id: logRes._id }, (err2) => {
+        if (err2 && callback) {
+          callback(err2);
+        } else {
+          callback(null);
         }
       });
     }
